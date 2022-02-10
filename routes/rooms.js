7464ddfb12d1807acf140ce1fcc0566a309401d4
kastellai <ilenia.castellana@gmail.com>
2022-02-10 12:58:19 +0100

    change values type in User and Room models add /room/:id/addlike with logic add /room/:id/removelike with logic add /room/:id/getlikes

--- routes/rooms.js
+++ routes/rooms.js
@@ -124,22 +124,25 @@ router.delete("/rooms/:id", async (req, res) => {
     .catch(error => res.status(500).json({ message: error }));
 });
 
-
+/**
+ * Add the like to the current room:
+ * - add the current userId to room.wholikesme
+ * - add the roomId to user.ilike
+ */
 router.patch("/rooms/:id/addlike", async (req, res) => {
   const roomId = req.params["id"];
 
   Room.findById({ _id: roomId })
     .then(result => {
       let wholikesme = result.wholikesme;
-      wholikesme = [...req.body.userId];
+      wholikesme.push(req.body.userId);
 
       Room.updateOne({ _id: roomId }, { $set: { wholikesme: wholikesme } })
-        .then(result => {
-          // res.status(200).json(result);
-          // roomId = ilike in localestorage + nuovo roomId
-          User.updateOne({ _id: req.body.userId }, { $set: { ilike: roomId } })
-            .then(result => {
-              res.status(200).json(result);
+        .then(_ => {
+          User.updateOne({ _id: req.body.userId }, { $set: { ilike: req.body.ilike } })
+            .then(_ => {
+              User.findById({ _id: req.body.userId }).then(userUpdated => res.status(200).json(userUpdated)
+              )
             });
         });
     })
@@ -148,17 +151,49 @@ router.patch("/rooms/:id/addlike", async (req, res) => {
         message: error
       });
     });
+});
 
+/**
+ * Removes the like from current room:
+ * - remove the current userId from room.wholikesme
+ * - remove the roomId from user.ilike
+ */
+router.patch("/rooms/:id/removelike", async (req, res) => {
+  const roomId = req.params["id"];
 
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  // {"propName": "wholikesme", "value": [previous + new userId]}
-
-  Room.updateOne({ _id: roomId }, { $set: updateOps })
+  Room.findById({ _id: roomId })
     .then(result => {
-      res.status(200).json(result);
+      let wholikesme = result.wholikesme;
+      wholikesme = wholikesme.filter(id => id !== req.body.userId)
+
+      Room.updateOne({ _id: roomId }, { $set: { wholikesme: wholikesme } })
+        .then(_ => {
+          User.updateOne({ _id: req.body.userId }, { $set: { ilike: req.body.ilike } })
+            .then(_ => {
+              User.findById({ _id: req.body.userId }).then(userUpdated => res.status(200).json(userUpdated)
+              )
+            });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error
+      });
+    });
+});
+
+/**
+ * Returns the list of users who have added the like the specific room
+ */
+router.get("/rooms/:id/getlikes", async (req, res) => {
+  const roomId = req.params["id"];
+
+  Room.findById({ _id: roomId })
+    .then(result => {
+      User.find().where({ '_id': { $in: result.wholikesme } })
+        .then(usersList => {
+          res.status(200).json(usersList);
+        });
     })
     .catch(error => {
       res.status(500).json({
